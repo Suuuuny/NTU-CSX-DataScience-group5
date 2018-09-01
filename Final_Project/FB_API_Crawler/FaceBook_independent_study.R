dfb7de260c6da84cc23a8a787e8cdc7c0bdd774d
dppss90008 <34399929+dppss90008@users.noreply.github.com>
2018-09-01 21:49:01 +0800

    upupupup

--- Final_Project/FB_API_Crawler/FaceBook_independent_study.R
+++ Final_Project/FB_API_Crawler/FaceBook_independent_study.R
@@ -5,121 +5,66 @@ library(ggplot2)
 
 # 丁守中 2017-12-17 - 2018-6-16
 Di_report <- read.csv("Di_report.csv")
-# 切開time變數
-Di_data <- Di_report %>% separate(time, c("date","time"), "T")
-Di_data <- Di_data %>% separate(date, c("year","month","day"), "-") 
-# 去除2017資料
-Di_data <- Di_data[Di_data$year == "2018", ]
-# 轉會為數值資料
-Di_data$month <- Di_data$month %>% as.numeric()
-Di_data$day <- Di_data$day %>% as.numeric()
-# 建立半月變數
-halfmonth <- c()
-for(i in Di_data$day>15) {
-  if (i==TRUE) {
-    halfmonth <- c(halfmonth,0.5)
-  }else{
-    halfmonth <- c(halfmonth,0)
-  }
-}
-# 半月變數加上原始月份，指定回資料表
-halfmonth <- Di_data$month + halfmonth
-halfmonth <- halfmonth %>% as.factor()
-summary(halfmonth)
-Di_data <- data.frame(Di_data,halfmonth)
 
-share_count = c()
-for (i in levels(Di_data$halfmonth)) {
-  share_count <- c(share_count,(Di_data[Di_data$halfmonth==i,7] %>% sum()))
-}
-### Share / 發文量 = CP值
-share_count_cp <- share_count / (summary(Di_data$halfmonth) %>% as.numeric())
-summary(Di_data$halfmonth) %>% as.numeric()
-# 視覺化
-month <- c("一月上","一月下","二月上","二月下","三月上","三月下","四月上","四月下","五月上","五月下","六月上","六月下")
-
-names(share_count_cp) <- levels(Di_data$halfmonth)
-share_count_cp <- share_count_cp %>% as.data.frame()
-colnames(share_count_cp) <- "share" 
-
-share_count_cp <- cbind(month, share_count_cp)
-
-Di_plot <- ggplot(share_count_cp, aes(month, share)) + 
-  geom_bar(stat = "identity", fill = "green3") +
-  ggtitle("丁守中發文分被享比例(每半月)") +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.05))
+# 建立 function 進行資料清理
+DataClean <- function(DATA){
+  # please input data frame
   
-
-
- # 姚文智 2017-11-15 - 2018-6-16
-Yao_report <- read.csv("Yao_report.csv")
-# 切開time變數
-Yao_data <- Yao_report %>% separate(time, c("date","time"), "T")
-Yao_data <- Yao_data %>% separate(date, c("year","month","day"), "-") 
-# 去除2017資料
-Yao_data <- Yao_data[Yao_data$year == "2018", ]
-# 轉會為數值資料
-Yao_data$month <- Yao_data$month %>% as.numeric()
-Yao_data$day <- Yao_data$day %>% as.numeric()
-# 建立半月變數
-halfmonth <- c()
-for(i in Yao_data$day>15) {
-  if (i==TRUE) {
-    halfmonth <- c(halfmonth,0.5)
-  }else{
-    halfmonth <- c(halfmonth,0)
+  # Cut time variable 
+  DATA <- DATA %>% separate(time, c("date","time"), "T")
+  DATA <- DATA %>% separate(date, c("year","month","day"), "-") 
+  # Drop 2017 data
+  DATA <- DATA[DATA$year == "2018", ]
+  # 轉換為數值資料
+  DATA$month <- DATA$month %>% as.numeric()
+  DATA$day <- DATA$day %>% as.numeric()
+  # Exchage month and day to halfmonth
+  halfmonth <- c()
+  for(i in DATA$day>15) {
+    if (i==TRUE) {
+      halfmonth <- c(halfmonth,0.5)
+    }else{
+      halfmonth <- c(halfmonth,0)
+    }
   }
+  # 半月變數加上原始月份，指定回資料表
+  halfmonth <- DATA$month + halfmonth
+  halfmonth <- halfmonth %>% as.factor()
+  # 把半月變數資料放回去data frame
+  DATA <- cbind(DATA,halfmonth)
+  # 清除6月半資料
+  DATA <- DATA[DATA$halfmonth!=6.5,]
+ 
+  return(DATA)
 }
-# 半月變數加上原始月份，指定回資料表
-halfmonth <- Yao_data$month + halfmonth
-halfmonth <- halfmonth %>% as.factor()
-summary(halfmonth)
-Yao_data <- data.frame(Yao_data,halfmonth)
 
-share_count = c()
-for (i in levels(Yao_data$halfmonth)) {
-  share_count <- c(share_count,(Yao_data[Yao_data$halfmonth==i,7] %>% sum()))
-}
-### Share / 發文量 = CP值
-share_count_cp <- share_count / (summary(Yao_data$halfmonth) %>% as.numeric())
-summary(Yao_data$halfmonth) %>% as.numeric()
+Di_report <- DataClean(Di_report)
 
-
-
-
-# 柯文哲 2017-10-22 - 2018-6-15
-Ko_report <- read.csv("Ko_report.csv")
-# 切開time變數
-Ko_data <- Ko_report %>% separate(time, c("date","time"), "T")
-Ko_data <- Ko_data %>% separate(date, c("year","month","day"), "-") 
-# 去除2017資料
-Ko_data <- Ko_data[Ko_data$year == "2018", ]
-# 轉會為數值資料
-Ko_data$month <- Ko_data$month %>% as.numeric()
-Ko_data$day <- Ko_data$day %>% as.numeric()
-# 建立半月變數
-halfmonth <- c()
-for(i in Ko_data$day>15) {
-  if (i==TRUE) {
-    halfmonth <- c(halfmonth,0.5)
-  }else{
-    halfmonth <- c(halfmonth,0)
+CountCP <- function(DATA,index){
+  # index = 7 : share, 8 : like 
+  count = c()
+  for (i in levels(DATA$halfmonth)[1:11]) {
+    count <- c(count,(DATA[DATA$halfmonth==i,index] %>% sum()))
   }
+  count_cp <- count / (summary(DATA$halfmonth)[1:11] %>% as.numeric())
+  month <- c("一月上","一月下","二月上","二月下","三月上","三月下","四月上","四月下","五月上","五月下","六月上") 
+  count_cp <- count_cp %>% as.data.frame()
+  count_cp <- cbind(month,count_cp)
+  count_cp$month <- count_cp$month %>% factor(.,levels = c("一月上","一月下","二月上","二月下","三月上","三月下","四月上","四月下","五月上","五月下","六月上"))
+  colnames(count_cp) <- c("month","CP")
+  return(count_cp)
 }
-# 半月變數加上原始月份，指定回資料表
-halfmonth <- Ko_data$month + halfmonth
-halfmonth <- halfmonth %>% as.factor()
-summary(halfmonth)
-Ko_data <- data.frame(Ko_data,halfmonth)
 
-share_count = c()
-for (i in levels(Ko_data$halfmonth)) {
-  share_count <- c(share_count,(Ko_data[Ko_data$halfmonth==i,7] %>% sum()))
-}
-### Share / 發文量 = CP值
-share_count_cp <- share_count / (summary(Ko_data$halfmonth) %>% as.numeric())
-summary(Ko_data$halfmonth) %>% as.numeric()
+Di_count_share_cp <- CountCP(Di_report,7)
+Di_count_like_cp <- CountCP(Di_report,8)
 
-# 視覺化
-names(share_count_cp) <- levels(Di_data$halfmonth)
+Di_share_plot <- ggplot(Di_count_share_cp, aes(x=month, y=CP)) + 
+  geom_bar(stat = "identity", fill = "green3") +
+  ggtitle("丁守中發文被分享比例(每半月)") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.05))
+
+Di_like_plot <- ggplot(Di_count_like_cp, aes(x=month, y=CP)) + 
+  geom_bar(stat = "identity", fill = "green3") +
+  ggtitle("丁守中發文被按讚比例(每半月)") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.05))
 
